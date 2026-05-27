056cacff2ba0771ce5bebad04ec53662c3991a26
Lucas Athié <luathie@gmail.com>
2026-05-27 13:42:27 -0300

    feat: JPA Repository, dependency injection, database seeding

---
+++ src/main/java/org/example/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package org.example.repositories;
+
+import org.example.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
