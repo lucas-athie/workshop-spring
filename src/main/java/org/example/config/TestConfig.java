aa764c3f65c2a418df4f124bf1363a41a0cbb08f
Lucas Athié <luathie@gmail.com>
2026-05-29 10:37:05 -0300

    feat: Payment entity one to one association

--- src/main/java/org/example/config/TestConfig.java
+++ src/main/java/org/example/config/TestConfig.java
@@ -55,6 +55,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
+
+
+
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
@@ -70,5 +74,10 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3, p4, p5));
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
